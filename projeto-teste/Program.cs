01b73b6fd4b01da6daa947498a51732bd497b794
Lucas Gabriel Chiaretti <lucaschiaretti@hotmail.com>
2020-04-16 22:41:05 -0300

    Acrescentada a mensagem desejando um bom dia

--- projeto-teste/Program.cs
+++ projeto-teste/Program.cs
@@ -4,9 +4,10 @@ namespace projeto_teste {
     class Program {
         static void Main(string[] args) {
 
-            Console.WriteLine("Olá mundo!");
-            Console.WriteLine("Bom Dia!");
+            Console.WriteLine("Olá mundo!");            
             Console.WriteLine("Como você está hoje?");
+            Console.WriteLine("Tenha um bom Dia!");
+
             Console.Read();
         }
     }
